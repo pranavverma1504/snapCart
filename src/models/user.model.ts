44ff2d212e1a945f2fc1243dcdb637a7ff01372a
pranav verma <vermapranav001@gmail.com>
2025-12-01 23:17:51 +0530

    created user model with its type (interface) using typescript in src/models/user.model.ts

---
+++ src/models/user.model.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+interface IUser {
+  _id?: mongoose.Types.ObjectId ;
+  name: string;
+  role: "admin" | "user" | "deliveryBoy";
+  email: string;
+  mobile?: string;
+  password?: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: false,
+      unique: true,
+      
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user", "deliveryBoy"],
+      default:"user",
+      required: true,
+    },
+    password: {
+      type: String,
+    },
+    
+  
+}, { timestamps: true })
+
+userSchema.index({location:'2dsphere'})
+const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+export default User;
