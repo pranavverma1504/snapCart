1f0655dac4db9dc9c3b932c69e5d1035fe90a4c2
pranav verma <vermapranav001@gmail.com>
2025-12-06 15:20:34 +0530

    created edit role and mobile no. page(component/EditRoleMobile) and when to show it in app/page.tsx  along with its API to update it on DB (api/user/editRoleMobile)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { auth } from "@/auth";
+import EditRoleMobile from "@/components/EditRoleMobile";
+import connectDb from "@/lib/db";
+import User from "@/models/user.model";
+import { redirect } from "next/navigation";
 
-const page = () => {
+export default async function Home() {
+  const session = await auth();
+  await connectDb();
+
+  if (!session?.user?.email) {
+    redirect("/login");
+  }
+
+
+
+  const user = await User.findOne({ email: session.user.email });
+  if (!user) redirect("/login");
+
+  
+  const isIncomplete =
+    !user.mobile ||
+    !user.role ||
+    (user.mobile === "0000000000" && user.role === "user");
+
+  if (isIncomplete) return <EditRoleMobile />;
+
+  const plainUser = JSON.parse(JSON.stringify(user));
+
   return (
-    <div>page</div>
-  )
+    <div className="flex flex-col min-h-screen bg-gray-50">
+     
+    </div>
+  );
 }
-
-export default page
